6ec6efb12d7a15114779ce756f9131922544b4ce
eray.akpinar <erayakpinar8@gmail.com>
2025-08-13 13:26:03 +0300

    DriverFactory.java dosyası popup göstermeyecek şekilde güncellendi.

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.Map;
+
 public class DriverFactory {
     private static WebDriver driver;
 
@@ -16,12 +18,21 @@ public class DriverFactory {
 
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--start-maximized");
-            options.addArguments("--disable-notifications"); // Bildirimleri kapatır
-            options.addArguments("--disable-popup-blocking"); // Popup engelleyici kapatılır
-            options.addArguments("--disable-save-password-bubble"); // Şifre kaydetme balonunu kapatır
-
-            // Çalıntı şifre uyarısını kapatmak için Chrome Preferences
+            options.addArguments("--disable-notifications");
+            options.addArguments("--disable-popup-blocking");
+            options.addArguments("--disable-save-password-bubble");
             options.addArguments("disable-features=PasswordLeakDetection");
+            options.addArguments("disable-infobars");
+
+            // Şifre yöneticisi ve otomatik doldurma popup'larını kapatır
+            options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation", "enable-logging"});
+            options.setExperimentalOption("prefs", Map.of(
+                    "credentials_enable_service", false,
+                    "profile.password_manager_enabled", false
+            ));
+
+            // İsteğe bağlı: misafir modunda başlatmak popup'ı engellemek için
+            // options.addArguments("--guest");
 
             driver = new ChromeDriver(options);
         }
